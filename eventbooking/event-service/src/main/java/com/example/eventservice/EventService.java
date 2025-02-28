6d499496ab5cda79b84be47b654b3f166aa6ad59
NNoorFatima <noor.2205.nf@gmail.com>
2025-02-28 18:05:31 +0500

    done please (SCRUM-5, SCRUM-4)

--- eventbooking/event-service/src/main/java/com/example/eventservice/EventService.java
+++ eventbooking/event-service/src/main/java/com/example/eventservice/EventService.java
@@ -4,7 +4,6 @@ import com.example.eventservice.model.Event;
 import com.example.eventservice.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 @Service
