8fc694100b07c79608829032947d597a1f7d26af
NNoorFatima <noor.2205.nf@gmail.com>
2025-02-28 16:07:22 +0500

    SCRUM-1, SCRUM-2, SCRUM-3 finished

--- eventbooking/user-service/app/crud.py
+++ eventbooking/user-service/app/crud.py
@@ -5,6 +5,7 @@ from app.models import User  # ✅ Correct
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
+#create a new user
 def create_user(db: Session, username: str, email: str, password: str):
     new_user = User(username=username, email=email, password=password)
     db.add(new_user)
